7300664e4f64bde72eb44036a1e93f19577a3306
Leonardo Christino <leomilho@gmail.com>
2016-06-25 18:55:53 -0300

    barril, esferas e física

---
+++ Assets/BarrelScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrelScript : MonoBehaviour {
+
+	public GameObject sphere;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			Destroy (sphere.GetComponent<FixedJoint> ());
+			sphere.GetComponent<Rigidbody> ().AddForce (transform.up * 800);
+
+		}
+	}
+}
---
+++ Assets/SphereScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		transform.position = col.transform.position;
+		Joint joint = gameObject.AddComponent <FixedJoint>();
+		joint.connectedBody = col.GetComponent<Rigidbody>();
+	}
+}
